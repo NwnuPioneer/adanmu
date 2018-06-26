83bc877d67a03f8c0fbd96a25371550870302b13
overdusk <overdusk@sina.com>
2018-06-26 22:15:56 +0800

    数据可视化

--- src/main/java/org/nwnu/system/controller/AnaySysController.java
+++ src/main/java/org/nwnu/system/controller/AnaySysController.java
@@ -1,9 +1,24 @@
 package org.nwnu.system.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.nwnu.base.controller.BaseController;
+import org.nwnu.system.entity.SysContent;
+import org.nwnu.system.entity.UserAnchor;
+import org.nwnu.system.service.SysContentService;
+import org.nwnu.system.service.UserAnchorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 
 /**
  * <p>
@@ -16,6 +31,10 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/AnaySys")
 public class AnaySysController extends BaseController  {
+	@Autowired
+	private UserAnchorService this_UserAnchorService;	
+	@Autowired
+	private SysContentService this_SysContentService;	
 	/***
 	 * 实时弹幕量的首页
 	 * 	 
@@ -50,5 +69,33 @@ public class AnaySysController extends BaseController  {
 	@RequestMapping(value = "/CipingIndex")
 	public ModelAndView cipingIndex(ModelAndView modelAndView) {
 		return modelAndView;
+	}	
+	/**
+	 * 主播弹幕统计
+	 * @return
+	 */
+	@RequestMapping(value = "/AchorIndex")
+	public String achorIndex() {		
+		return "AnaySys/AchorIndex";
+	}
+	/**
+	 * 获取主播弹幕统计
+	 */
+	@RequestMapping(value = "/getAnchorList", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> getAnchorList() {
+		List<UserAnchor> anchorList=this_UserAnchorService.selectList(new EntityWrapper<UserAnchor>());
+		String[] acnhor=new String[anchorList.size()];
+		int[] number=new int[anchorList.size()];
+		int i=0;
+		for(UserAnchor anchor:anchorList){
+			acnhor[i]=anchor.getName();
+			number[i]=this_SysContentService.selectList(new EntityWrapper<SysContent>().eq("pid",anchor.getRid().toString())).size();
+			i++;
+		}
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("acnhor", acnhor);
+		result.put("number", number);
+		return result;
 	}	
 }
